a8cf4d3c81efa514b82cbb31c75d05d9814f9397
Balpoff <78598763+Balpoff@users.noreply.github.com>
2021-11-23 10:12:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from SerialCommand import Com
 
-s = Com(ARDPath="COM4", portSpeed=115200)
+s = Com(ARDPath="/dev/ttyUSB0", portSpeed=115200)
 
 while True:
-    s.writeCmd_(0)  # from 90 to 255
+    i = int(input())
+    s.writeCmd_(i)  # from 90 to 255
